16719f41714164add2ba6b1adb2ba0562d91c66c
sparkrico <sparkrico@deluodedeMacBook-Pro.local>
2018-02-01 16:32:08 +0800

    init

---
+++ app/src/main/java/vudroid/org/vudroid/MainActivity.java
@@ -0,0 +1,41 @@
+package vudroid.org.vudroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import org.vudroid.core.DocumentView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "666", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        DocumentView documentView;
+    }
+
+}
---
+++ lib/src/main/java/org/vudroid/codec/PdfContext.java
@@ -0,0 +1,26 @@
+package org.vudroid.codec;
+
+import android.content.ContentResolver;
+
+import org.vudroid.core.VuDroidLibraryLoader;
+import org.vudroid.core.codec.CodecContext;
+import org.vudroid.core.codec.CodecDocument;
+
+import java.io.Serializable;
+
+public class PdfContext implements CodecContext, Serializable {
+    static {
+        VuDroidLibraryLoader.load();
+    }
+
+    public CodecDocument openDocument(String fileName) {
+        return PdfDocument.openDocument(fileName, "");
+    }
+
+    public void setContentResolver(ContentResolver contentResolver) {
+        //TODO
+    }
+
+    public void recycle() {
+    }
+}
